d982f82cdf1c35b7828ff77505549e5473a399be
유성윤 <94370559+kaje033@users.noreply.github.com>
2024-04-09 08:45:56 +0900

    [level 0] Title: l로 만들기, Time: 1.11 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        return myString.replace("a", "l").replace("b", "l")
+                        .replace("c", "l").replace("d", "l")
+                        .replace("d", "l").replace("e", "l")
+                        .replace("f", "l").replace("g", "l")
+                        .replace("h", "l").replace("i", "l")
+                        .replace("j", "l").replace("k", "l");
+    }
+}
