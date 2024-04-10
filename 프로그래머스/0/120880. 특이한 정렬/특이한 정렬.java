2e0d1dc171db54a6073ca7aaba7374c2aaba1633
유성윤 <94370559+kaje033@users.noreply.github.com>
2024-04-11 01:46:44 +0900

    [level 0] Title: 특이한 정렬, Time: 3.36 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        return Arrays.stream(numlist)
+                .boxed()
+                .sorted((a, b) -> Math.abs(a - n) == Math.abs(b - n)
+                        ? b.compareTo(a) 
+                        : Integer.compare(Math.abs(a - n), Math.abs(b - n)))
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
