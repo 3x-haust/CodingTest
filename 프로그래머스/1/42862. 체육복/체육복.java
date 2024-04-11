0f0ea15c65cfed1691f8db7ac9f7e505e5eeda1a
유성윤 <94370559+kaje033@users.noreply.github.com>
2024-04-11 21:34:34 +0900

    [level 1] Title: 체육복, Time: 0.32 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        // 여벌 체육복 - 도난
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1; 
+                    
+                    break; 
+                }
+            }
+        }
+        // 도난당한 학생 - 빌려줌
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if((lost[i] - 1 == reserve[j]) || (lost[i] + 1 == reserve[j])){
+                    answer++;
+                    reserve[j] = -1; 
+                    
+                    break; 
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
