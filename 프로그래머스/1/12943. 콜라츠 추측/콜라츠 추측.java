ebca43d0d362c70c7d994f046f5f453277883f06
유성윤 <94370559+kaje033@users.noreply.github.com>
2024-04-11 12:56:07 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(long num) {
+        long answer = 0;
+        
+        while(num != 1) {
+            if(num % 2 == 0)
+                num /= 2;
+            else
+                num = num * 3 + 1;
+            answer++;
+            
+            if(answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+        
+        return (int) answer;
+    }
+}
