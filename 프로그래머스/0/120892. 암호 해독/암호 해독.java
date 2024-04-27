4b189fdc9914bec815527780533f57f8cffe54c7
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-04-28 02:28:17 +0900

    [level 0] Title: 암호 해독, Time: 2.44 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,9 +1,5 @@
 class Solution {
-    public String solution(String cipher, int code) {
-        String answer = "";
-        for (int i = 1; i <= cipher.length() / code; i++){
-            answer += String.valueOf(cipher.charAt(code * i - 1));
-        }
-        return answer;
+    public String solution(String cipher, int code) {    
+        return java.util.stream.IntStream.range(1, cipher.length() / code + 1).mapToObj(i -> String.valueOf(cipher.charAt(code * i - 1))).collect(java.util.stream.Collectors.joining());
     }
 }
