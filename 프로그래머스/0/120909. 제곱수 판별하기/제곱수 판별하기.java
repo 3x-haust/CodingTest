d36f1322e477a6db9be5173631eb06638ef2f9a9
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-04-28 02:35:05 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int n) {
-        long root = (long) Math.sqrt(n);
-        
-        return n == Math.pow(root, 2) ? 1 : 2;
+        return n == Math.pow((long) Math.sqrt(n), 2) ? 1 : 2;
     }
 }
