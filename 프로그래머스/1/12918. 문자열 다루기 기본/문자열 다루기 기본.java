437fa9cf5a67b92c390f1279636d2d18eeaffbb6
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-09-23 01:48:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        return s.length() == 4 || s.length() == 6 ? s.matches("^[0-9]+$") ? true : false : false;
+        return s.matches("\\d{4}|\\d{6}"); 
     }
 }
