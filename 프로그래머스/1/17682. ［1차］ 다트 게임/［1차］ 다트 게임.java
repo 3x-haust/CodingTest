7a485b111a47b3857bcfecec4e2acb8e80578f18
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-09-23 03:17:07 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.67 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,43 +1,24 @@
 class Solution {
-    public int solution(String dartResult) {
-        int answer = 0;
-        int[] dart = new int[3];
+    public static int solution(String dartResult) {
+        int[] scores = new int[3];
+        String[] turns = dartResult.split("(?<=\\D)(?=\\d)");
 
-        int n = 0, index = 0;
-        String numstr = "";
+        for (int i = 0; i < turns.length; i++) {
+            String turn = turns[i];
+            int score = Integer.parseInt(turn.replaceAll("\\D", ""));
+            score = turn.contains("D") ? score * score : turn.contains("T") ? score * score * score : score;
+
+            if (turn.contains("*")) {
+                score *= 2;
+                if (i > 0) scores[i - 1] *= 2;
+            }
+            
+            if (turn.contains("#")) score *= -1;
         
-        for(int i = 0; i < dartResult.length(); i++){
-            char c = dartResult.charAt(i);
-            
-            //숫자
-            if(c >= '0' && c <= '9'){       
-                 numstr += String.valueOf(c);
-            }
-            
-            //보너스
-            else if(c == 'S' || c == 'D' || c == 'T'){
-                n = Integer.parseInt(numstr);
-                
-                if(c=='S') dart[index++] = (int) Math.pow(n, 1);
-                else if(c=='D') dart[index++] = (int) Math.pow(n, 2);
-                else dart[index++] = (int) Math.pow(n, 3);
-                
-                numstr = "";
-            }
-            
-            //옵션
-            else {
-                if(c == '*'){
-                    dart[index - 1] *= 2;
-                    if(index - 2 >= 0) dart[index - 2] *= 2; 
-                }
-                else dart[index - 1] *= -1;
-                
-            }
+
+            scores[i] = score;
         }
-    
-        answer=dart[0]+dart[1]+dart[2];
-    
-        return answer;
+
+        return java.util.Arrays.stream(scores).sum();
     }
 }
